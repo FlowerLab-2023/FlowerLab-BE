bdd0b2f1ae20abe2fc1742f2c98b03f16827de69
suddiyo <suddiyo@naver.com>
2023-10-18 14:05:34 +0900

    initial commit

---
+++ src/main/java/com/example/flowerlab/FlowerlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowerlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowerlab/FlowerlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowerlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
